72aa81a6db478b29f36cdf57098f1e76a1b28888
Maria Ragone <maria@ragone.net>
2023-08-14 21:23:44 -0400

    first commit

---
+++ Deck of Cards Lab/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Deck_of_Cards_Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Deck_of_Cards_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[HttpGet]
+        //public IActionResult Play()
+        //{
+
+        //    return View();
+        //}
+        //[HttpPost]
+        public IActionResult Play()
+        {
+            DeckofCardsModel result = DeckofCardsDAL.Draw5Cards();
+            if (result.remaining <= 4)
+
+            {
+                DeckofCardsDAL.Reshuffle();
+            }
+            return View(result);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Deck of Cards Lab/Models/DeckofCardsDAL.cs
@@ -0,0 +1,52 @@
+﻿using static System.Net.WebRequestMethods;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Deck_of_Cards_Lab.Models
+{
+    public class DeckofCardsDAL
+    {
+        public static DeckofCardsModel Draw5Cards()//adjust- use correct model, update method name
+        {	//adjust
+            //setup
+            string deckID = "i6bdww7srht5";
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count=5";//link to Deck ID
+
+            //request data
+            HttpWebRequest request = WebRequest.CreateHttp(url); //takes the url and calls the website and saves the call
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); //saves the response
+            //converting to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd(); //takes all the info and saves it into a single string to use in our objects
+
+            //adjust
+            //converting to C#
+            //right click on JsonConvert. > install Newtonsoft.json
+            DeckofCardsModel result = JsonConvert.DeserializeObject<DeckofCardsModel>(json);//json is the string name
+            return result;
+
+        }
+
+        public static DeckofCardsModel Reshuffle()//adjust- use correct model, update method name
+        {	//adjust
+            //setup
+            string deckID = "i6bdww7srht5";
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/shuffle/";//link to Deck ID
+
+            //request data
+            HttpWebRequest request = WebRequest.CreateHttp(url); //takes the url and calls the website and saves the call
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); //saves the response
+            //converting to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd(); //takes all the info and saves it into a single string to use in our objects
+
+            //adjust
+            //converting to C#
+            //right click on JsonConvert. > install Newtonsoft.json
+            DeckofCardsModel result = JsonConvert.DeserializeObject<DeckofCardsModel>(json);//json is the string name
+            return result;
+
+        }
+
+    }
+}
